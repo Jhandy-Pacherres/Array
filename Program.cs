982e4f11e963c80a502904e632daddebe64e2691
Jhandy-Pacherres <jhandyfloresf@gmail.com>
2026-09-23 10:12:27 -0500

    Calcular promedio

--- Program.cs
+++ Program.cs
@@ -9,10 +9,28 @@ static void imprimirNotas (int[] notas)
 }
 
 
-
-
-
 int[] notas = {15 , 18 , 12 , 17 , 20};
 
 // Imprimir las notas
 imprimirNotas(notas);
+
+
+//calcular el promedio de las notas
+
+
+static double promedioNotas(int[] notas)
+{
+    int suma = 0;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        suma += notas[i];
+    }
+
+    double promedio = (double)suma / notas.Length;
+
+    return promedio;
+}
+
+double promedio = promedioNotas(notas);
+Console.WriteLine($"El promedio es: {promedio}");
