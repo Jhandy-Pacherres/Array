bdf74c461430907a6d4216a91902e6fb2a4d4fee
Jhandy-Pacherres <jhandyfloresf@gmail.com>
2026-09-23 09:13:40 -0500

    Impresion de notas funcion

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿int[] notas = {15 , 18 , 12 , 17 , 20};
-
-// Imprimir las notas
-for (int i = 0 ; i<notas.Length ; i++)
+﻿//funcion de imprimir
+static void imprimirNotas (int[] notas)
+{
+    for (int i = 0 ; i<notas.Length ; i++)
 {
     System.Console.WriteLine(notas [i]);
 }
+
+}
+
+
+
+
+
+int[] notas = {15 , 18 , 12 , 17 , 20};
+
+// Imprimir las notas
+imprimirNotas(notas);
